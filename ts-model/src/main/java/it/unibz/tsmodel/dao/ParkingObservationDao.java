708b5f5251b5a30e67f46e87699138a2e6a7ef42
bertolla <p.bertolla@noi.bz.it>
2019-03-25 14:35:02 +0100

    add query to retrieve last entry for specific parking lot

--- ts-model/src/main/java/it/unibz/tsmodel/dao/ParkingObservationDao.java
+++ ts-model/src/main/java/it/unibz/tsmodel/dao/ParkingObservationDao.java
@@ -1,42 +1,60 @@
 package it.unibz.tsmodel.dao;
 
-import it.unibz.tsmodel.domain.ParkingObservation;
-
 import java.sql.Timestamp;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import it.unibz.tsmodel.domain.ParkingObservation;
+
 @Repository
 public class ParkingObservationDao {
-	
+
 	@PersistenceContext
 	private EntityManager entityManager;
-	
+
 	/**
-	 *@return the list of {@link ParkingObservation} with the specified parking id and after 
+	 *@return the list of {@link ParkingObservation} with the specified parking id and after
 	 *the specified date present in the DB
 	 */
 	public List<ParkingObservation> findObservationsByPlace(String pid, Timestamp afterThat) {
-		String query = "SELECT (o) " + 
-                "FROM ParkingObservation o " + 
+		String query = "SELECT (o) " +
+                "FROM ParkingObservation o " +
                 "WHERE o.parkingplace = :pid AND o.timestamp > :afterThat"+
                 " ORDER BY timestamp";
-		
+
 		TypedQuery<ParkingObservation> typedQuery = entityManager.createQuery(query, ParkingObservation.class);
 		typedQuery.setParameter("pid", pid);
 		typedQuery.setParameter("afterThat", afterThat);
 		return  typedQuery.getResultList();
    }
-	
+
 	@Transactional
 	public void persist(ParkingObservation observation) {
 		entityManager.persist(observation);
+	}
+
+	public ParkingObservation findNewestObservationsByPlace(String parkingId) {
+				String query = "SELECT (o) " +
+                "FROM ParkingObservation o " +
+                "WHERE o.parkingplace = :pid"+
+                " ORDER BY o.timestamp desc";
+
+		TypedQuery<ParkingObservation> typedQuery = entityManager.createQuery(query, ParkingObservation.class).setMaxResults(1);
+		typedQuery.setParameter("pid", parkingId);
+		ParkingObservation singleResult = null;
+		try{
+			singleResult = typedQuery.getSingleResult();
+		}catch(NoResultException ex) {
+			ex.printStackTrace();
+		}
+		return singleResult;
 	}
 
 
