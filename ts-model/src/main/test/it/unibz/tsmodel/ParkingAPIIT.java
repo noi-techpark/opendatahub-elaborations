1ab96695450ec2fd90aa96b9339d67fa42b48093
bertolla <p.bertolla@noi.bz.it>
2019-03-25 09:49:34 +0100

    [ts-model] correctly renaming of integrationtest

--- ts-model/src/main/test/it/unibz/tsmodel/ParkingAPIIT.java
+++ ts-model/src/main/test/it/unibz/tsmodel/ParkingAPIIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import it.unibz.tsmodel.forecast.ForecastCron;
 @ContextConfiguration(locations = { "classpath:/META-INF/spring/applicationContext.xml" })
-public class ParkingAPITest extends AbstractJUnit4SpringContextTests{
+public class ParkingAPIIT extends AbstractJUnit4SpringContextTests{
 
 	@Autowired
 	private ForecastCron cron;
