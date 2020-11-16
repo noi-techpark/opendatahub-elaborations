5cce4134dbf871e6b39a83e1975c81aa17ae8b7a
bertolla <p.bertolla@noi.bz.it>
2020-11-16 17:31:10 +0100

    add some logging

--- airquality-elaborations/src/main/java/it/bz/odh/elaborations/services/JobScheduler.java
+++ airquality-elaborations/src/main/java/it/bz/odh/elaborations/services/JobScheduler.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.time.DateUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,8 @@ import it.bz.idm.bdp.dto.SimpleRecordDto;
 
 @Service
 public class JobScheduler {
+
+    private Logger logger = LogManager.getLogger(JobScheduler.class);
 
     @Autowired
     private ODHParser odhParser;
@@ -29,9 +33,15 @@ public class JobScheduler {
 
     public void executeElaborations() {
 
+        logger.info("Start retrieving data");
         DataMapDto<RecordDtoImpl> dataMap = odhParser.createDataMap();
+        if (!dataMap.getBranch().isEmpty())
+            logger.info("Successfully created non empty Raw datatype map");
+        else
+            logger.info("Got no data to create a Raw datatype map");
+        logger.info("Create elaboration map with newest elaboration timestamps");
         DataMapDto<RecordDtoImpl> newestElaborationMap = odhParser.createNewestElaborationMap();
-
+        logger.info("Start iteration through tree to do elaborations");
         for (Map.Entry<String, DataMapDto<RecordDtoImpl>> stationMapEntry : dataMap.getBranch().entrySet()) {
             for (Map.Entry<String, DataMapDto<RecordDtoImpl>> typeMapEntry : stationMapEntry.getValue().getBranch().entrySet()) {
                 List<RecordDtoImpl> data = new ArrayList<RecordDtoImpl>();
@@ -60,17 +70,21 @@ public class JobScheduler {
             }
             else
                 stationData = odhParser.getStationData(station,type);
+            logger.info("Create elaboration map with newest elaboration timestamps");
             if (!stationData.isEmpty()) {
                 List<SimpleRecordDto> averageElaborations = elaborationService.calcAverage(stationData,Calendar.HOUR);
                 if (!averageElaborations.isEmpty()) {
                     DataMapDto<RecordDtoImpl> dto = new DataMapDto<RecordDtoImpl>();
                     dto.addRecords(station, type, averageElaborations);
-
+                    logger.debug("Created "+averageElaborations.size()+" elaborations for "+station+":"+type);
                     webClient.pushData(dto);
+                    logger.debug("Completed sending to odh");
                     Long newOldestElaboration = DateUtils.ceiling(new Date(averageElaborations.get(averageElaborations.size()-1).getTimestamp()),Calendar.HOUR).getTime();
                     if (newOldestElaboration <= lastRawDateUTC)
                         startElaboration(station, type, newOldestElaboration, lastRawDateUTC);
                 }
+            }else {
+                logger.debug("Unable to get raw data for "+station+":"+type);
             }
         } catch (ParseException e) {
             e.printStackTrace();
