bf911953bae0dcbff2dacef98862852fb8defa27
bertolla <p.bertolla@noi.bz.it>
2020-12-01 17:10:42 +0100

    add docker infrastructure

--- FreeParkingSlotsCalculation/model/Dtos.py
+++
@@ -1,3 +0,0 @@
-class DataPoint(dict):
-    def __init__(self,timestamp,value,period):
-        dict.__init__(self, timestamp=timestamp, value=value, period=period,_t="it.bz.idm.bdp.dto.SimpleRecordDto")
