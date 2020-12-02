a62033b25381c8d8a2f748874f12bb789adc21df
bertolla <p.bertolla@noi.bz.it>
2020-12-02 14:45:45 +0100

    fix typo

--- FreeParkingSlotsCalculation/infrastructure/Jenkinsfile-Test.groovy
+++ FreeParkingSlotsCalculation/infrastructure/Jenkinsfile-Test.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'FreeParkingSlotsCalculations/infrastructure/Dockerfile'
+            filename 'FreeParkingSlotsCalculation/infrastructure/Dockerfile'
             additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
         }
     }
