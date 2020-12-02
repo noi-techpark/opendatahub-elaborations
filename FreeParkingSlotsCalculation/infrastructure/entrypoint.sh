b78d6ed55d40b5fdac79597aaff52f0c9adc80fa
bertolla <p.bertolla@noi.bz.it>
2020-12-02 14:25:35 +0100

    change pipeline from scratch

--- FreeParkingSlotsCalculation/infrastructure/entrypoint.sh
+++ FreeParkingSlotsCalculation/infrastructure/entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 pip install --no-cache-dir -r requirements.txt -t ./src
-cd src && zip -r /data/freeParkingSlotsCalculator.zip .
-python -c 'import lambdaInit;lambdaInit.handleLambdaCall(1,2)'
+cd src && zip -r /data/freeParkingSlotsCalculator.zip . && python -c 'import lambdaInit;lambdaInit.handleLambdaCall(1,2)'
