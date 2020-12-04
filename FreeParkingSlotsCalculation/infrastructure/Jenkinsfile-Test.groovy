b8afd29257847dd0dbc50f768776230a41ed80b0
bertolla <p.bertolla@noi.bz.it>
2020-12-04 10:58:10 +0100

    use single docker file

--- FreeParkingSlotsCalculation/infrastructure/Jenkinsfile-Test.groovy
+++ FreeParkingSlotsCalculation/infrastructure/Jenkinsfile-Test.groovy
@@ -2,7 +2,6 @@ pipeline {
     agent {
         dockerfile {
             filename 'FreeParkingSlotsCalculation/infrastructure/Dockerfile'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins` --build-arg PROJECT_FOLDER="FreeParkingSlotsCalculation" --build-arg BUILD_BUNDLE="freeParkingSlotsCalculator.zip"'
         }
     }
 
@@ -10,17 +9,17 @@ pipeline {
         PROJECT_FOLDER="FreeParkingSlotsCalculation"
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
-        AUTHENTICATION_SERVER='https://auth.opendatahub.testingmachine.eu/auth/'
-        CLIENT_SECRET= credentials('keycloak-datacollectors-secret')
-        ODH_SHARE_ENDPOINT='https://share.opendatahub.testingmachine.eu/'
-        RAW_DATA_ENDPOINT='https://mobility.api.opendatahub.testingmachine.eu/v2'
-        PROVENANCE_NAME="FreeParkingSlotsCalculation"
-        PROVENANCE_VERSION="0.1.0"
-        PROVENANCE_LINEAGE="noi"
+        AWS_DEFAULT_REGION="eu-west-1"
         BUILD_BUNDLE="freeParkingSlotsCalculator.zip"
     }
 
     stages {
+        stage('Build') {
+            steps {
+                sh 'cd ${PROJECT_FOLDER} && pip install --no-cache-dir -r requirements.txt -t ./src'
+                sh "cd ${PROJECT_FOLDER}/src && zip -r ../${BUILD_BUNDLE} ."
+            }
+        }
         stage('Upload') {
             steps {
                 sh 'aws lambda update-function-code --function-name freeParkingLotsElaborations --zip-file fileb://${PROJECT_FOLDER}/${BUILD_BUNDLE}'
