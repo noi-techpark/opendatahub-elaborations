09a05d0ae0172accf5bae7542bb42c614cc077ad
bertolla <p.bertolla@noi.bz.it>
2021-07-13 16:41:33 +0200

    add testing pipeline

--- Environment-A22-Processing/infrastructure/Jenkinsfile-Test.groovy
+++ Environment-A22-Processing/infrastructure/Jenkinsfile-Test.groovy
@@ -1,21 +1,21 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'FreeParkingSlotsCalculation/infrastructure/Dockerfile'
+            filename 'Environment-A22-Processing/infrastructure/Dockerfile'
         }
     }
 
     environment {
-        PROJECT_FOLDER="FreeParkingSlotsCalculation"
+        PROJECT_FOLDER="Environment-A22-Processing"
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
         AWS_DEFAULT_REGION="eu-west-1"
-        BUILD_BUNDLE="freeParkingSlotsCalculator.zip"
+        BUILD_BUNDLE="dataprocessing.zip"
 	    AUTHENTICATION_SERVER='https://auth.opendatahub.testingmachine.eu/auth/'
         CLIENT_SECRET= credentials('test-keycloak-lambda-secret')
         ODH_SHARE_ENDPOINT='https://share.opendatahub.testingmachine.eu/'
         RAW_DATA_ENDPOINT='https://mobility.api.opendatahub.testingmachine.eu/v2'
-        PROVENANCE_NAME="FreeParkingSlotsCalculation"
+        PROVENANCE_NAME="dataprocessing-a22-environment"
         PROVENANCE_VERSION="0.1.0"
         PROVENANCE_LINEAGE="NOI"
     }
@@ -29,7 +29,7 @@ pipeline {
         }
         stage('Upload') {
             steps {
-                sh """aws lambda update-function-configuration --function-name freeParkingLotsElaborations \
+                sh """aws lambda update-function-configuration --function-name a22DataProcessing \
 		        --environment Variables={AUTHENTICATION_SERVER=${AUTHENTICATION_SERVER},CLIENT_SECRET=${CLIENT_SECRET},ODH_SHARE_ENDPOINT=${ODH_SHARE_ENDPOINT},RAW_DATA_ENDPOINT=${RAW_DATA_ENDPOINT},PROVENANCE_NAME=${PROVENANCE_NAME},PROVENANCE_VERSION=${PROVENANCE_VERSION},PROVENANCE_LINEAGE=${PROVENANCE_LINEAGE}} """
                 sh 'aws lambda update-function-code --function-name freeParkingLotsElaborations --zip-file fileb://${PROJECT_FOLDER}/${BUILD_BUNDLE}'
             }
