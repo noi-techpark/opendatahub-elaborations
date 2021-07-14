f59b7b24950cf706fcf85501408d94d6b7babfa4
bertolla <p.bertolla@noi.bz.it>
2021-07-14 14:54:17 +0200

    use correct env var

--- Environment-A22-Processing/src/ODHAPIClient.py
+++ Environment-A22-Processing/src/ODHAPIClient.py
@@ -13,9 +13,7 @@ class DataFetcher:
         return stationMap
 
     def fetch_data(self, endpoint):
-        print(endpoint)
-        print(self.token)
-        r = requests.get(os.getenv("ODH_API_ENDPOINT") + endpoint,headers={"Authorization" : "Bearer " + self.token['access_token']})
+        r = requests.get(os.getenv("RAW_DATA_ENDPOINT") + endpoint,headers={"Authorization" : "Bearer " + self.token['access_token']})
         if (r.status_code != 200):
             print("Status code not 200 but " + str(r.status_code))
         else: 
