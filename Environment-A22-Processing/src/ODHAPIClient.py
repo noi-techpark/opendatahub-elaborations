d4d119f2df3f9fc1eb9dc3944929a6de13fd523a
bertolla <p.bertolla@noi.bz.it>
2021-07-15 09:27:36 +0200

    add some logging

--- Environment-A22-Processing/src/ODHAPIClient.py
+++ Environment-A22-Processing/src/ODHAPIClient.py
@@ -1,21 +1,21 @@
 import requests
 import os
+import logging
 from ODHKeyCloakClient import KeycloakClient
+
+log = logging.getLogger()
+logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO"))
 
 class DataFetcher:
     def __init__(self):
         self.token = KeycloakClient.getDefaultInstance().token("", "","client_credentials")
-
-    def create_data_map(self,station,dataType,dataPoints):
-        dataMap = {"name":"(default)","branch":{},"data": dataPoints}
-        typeMap = {"name":"(default)","branch": {dataType:dataMap},"data":[]}
-        stationMap = {"name":"(default)","branch":{station:typeMap},"data":[], "provenance": self.provenanceId}
-        return stationMap
+        log.debug("Token created:")
+        log.debug(self.token)
 
     def fetch_data(self, endpoint):
         r = requests.get(os.getenv("RAW_DATA_ENDPOINT") + endpoint,headers={"Authorization" : "Bearer " + self.token['access_token']})
         if (r.status_code != 200):
-            print("Status code not 200 but " + str(r.status_code))
+            log.warn("Status code not 200 but " + str(r.status_code))
         else: 
             return r.json()
 
@@ -23,6 +23,7 @@ class DataFetcher:
         response = self.fetch_data(
             "/tree/EnvironmentStation/*/latest?limit=-1&where=sactive.eq.true,sorigin.eq.a22-algorab,mperiod.eq.3600&select=mvalidtime,scode&limit=-1")
         if (response != None):
+            log.debug("fetched newest data for raw and processed data:" + str(response))
             stations = response['data']['EnvironmentStation']['stations']
             station_map = {}
             for station_id in stations:                
@@ -35,21 +36,25 @@ class DataFetcher:
                     state = type_map.get(type_split[0],{})
                     state[type_split[1]] = newest_record_timestamp
                     type_map[type_split[0]] = state
+            log.debug("Generated station map: " + str(station_map))
             return station_map
 
     def get_raw_history(self, station_id, start, end):
-        elab_map = {}
+        raw_data_map = {}
         data = self.fetch_data( "/flat/EnvironmentStation/*/" 
         + str(start).replace(" ","T") + "/"+ str(end).replace(" ","T") 
         + "?limit=-1&where=sactive.eq.true,sorigin.eq.a22-algorab,mperiod.eq.3600,scode.eq."
         + station_id + "&select=mvalidtime,mvalue,tname")['data']
-        for record in data:
-            type_arr = str(record['tname']).split("_")
-            if len(type_arr)>1 and type_arr[1] == "raw":
-                value = record['mvalue']
-                type_id = type_arr[0]
-                time = record['mvalidtime']
-                typeMap = {}
-                typeMap[type_id] = value 
-                elab_map.setdefault(time,{}).update(typeMap)
-        return elab_map
+        if data != None:
+            log.debug("fetched history data:" + str(data))
+            for record in data:
+                type_arr = str(record['tname']).split("_")
+                if len(type_arr)>1 and type_arr[1] == "raw":
+                    value = record['mvalue']
+                    type_id = type_arr[0]
+                    time = record['mvalidtime']
+                    typeMap = {}
+                    typeMap[type_id] = value 
+                    raw_data_map.setdefault(time,{}).update(typeMap)
+        log.debug("Raw history: " + str(raw_data_map))
+        return raw_data_map
--- Environment-A22-Processing/src/ODHPusher.py
+++ Environment-A22-Processing/src/ODHPusher.py
@@ -1,7 +1,10 @@
 import requests
 import os
+import logging
 from model.Dtos import Provenance
 from ODHKeyCloakClient import KeycloakClient
+
+log = logging.getLogger()
 
 class DataPusher:
     def __init__(self):
@@ -12,15 +15,19 @@ class DataPusher:
     def send_data(self,station_type, data_map):
         data_map["provenance"]= self.provenance_id
         endpoint = os.getenv("ODH_SHARE_ENDPOINT")+"/json/pushRecords/" + station_type
+        log.debug("Data send to writer: " + str(data_map))
         r = requests.post(endpoint, json=data_map, headers={"Authorization" : "Bearer " + self.token['access_token']})
         if (r.status_code != 201):
-            print("Status code not 201 but " + str(r.status_code))
-            print(data_map)
+            log.warn("Status code not 201 but " + str(r.status_code))
+            log.warn(data_map)
     
     def upsert_provenance(self):
         collector = os.getenv("PROVENANCE_NAME")
+        log.debug("Provenance name: " + collector)
         version = os.getenv("PROVENANCE_VERSION")
+        log.debug("Provenance verison: " + version)
         lineage = os.getenv("PROVENANCE_LINEAGE")
+        log.debug("Provenance lineage: " + lineage)
         p = Provenance(None, lineage, collector, version)
         r = requests.post(os.getenv("ODH_SHARE_ENDPOINT")+"/json/provenance", json= p, headers={"Authorization" : "Bearer " + self.token['access_token']})
         self.provenance_id = r.text
