20366770debc382201cd3747be036c4ed517520a
Peter Moser <p.moser@noi.bz.it>
2022-06-10 16:56:33 +0200

    environment-a22-non-linear-calibration: add CD for testing / 10

--- environment-a22-non-linear-calibration/env_to_simple_json.py
+++ environment-a22-non-linear-calibration/env_to_simple_json.py
@@ -8,11 +8,8 @@ prefix = "LAMBDA_"
 output = []
 
 
-print("{", end="")
 for key in env_keys:
     if key.startswith(prefix):
-        print(f"{key.split(prefix, 1)[1]}={os.environ.get(key)}", end="")
-print("}", end="")
+        output.append(f"{key.split(prefix, 1)[1]}={os.environ.get(key)}", end="")
 
-# if output:
-#     print(f'{{{",".join(output)}}}')
+print(f'{{{",".join(output)}}}')
