0f7afcc9f42dc1c30ac35dc064d3adc8434b15c4
Clemens Zagler <c.zagler@noi.bz.it>
2023-03-01 10:35:50 +0100

    env-a22-non-linear-cal: migrated from aws lambda to docker

--- environment-a22-non-linear-calibration/src/lambdaInit.py
+++
@@ -1,6 +0,0 @@
-from dataprocessor.DataProcessor import Processor
-
-f = Processor()
-
-def handleLambdaCall(event,arguments):
-    f.calc_by_station()
---
+++ environment-a22-non-linear-calibration/src/main.py
@@ -0,0 +1,10 @@
+from dataprocessor.DataProcessor import Processor
+
+f = Processor()
+
+def main():
+    f.calc_by_station();
+
+if __name__ == "__main__":
+    main()
+    
