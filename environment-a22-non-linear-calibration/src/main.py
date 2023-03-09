7f8427e8bf764c7c1f98fc501758f8ec78eb4287
Clemens Zagler <c.zagler@noi.bz.it>
2023-03-09 16:05:47 +0100

    env-a22-nlc: add start/stop logging so that there is at least some output that indicates the job is working

--- environment-a22-non-linear-calibration/src/main.py
+++ environment-a22-non-linear-calibration/src/main.py
@@ -1,9 +1,13 @@
 from dataprocessor.DataProcessor import Processor
+import logging
 
 f = Processor()
+log = logging.getLogger()
 
 def main():
+    log.info('Elaboration start')
     f.calc_by_station();
+    log.info('Elaboration end')
 
 if __name__ == "__main__":
     main()
