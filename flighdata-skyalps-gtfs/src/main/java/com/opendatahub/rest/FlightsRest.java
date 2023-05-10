837d33b1c25e86a9870b0ad2e17739d1dae2648d
Simon Dalvai <s.dalvai@noi.bz.it>
2023-05-10 13:17:13 +0200

    flighdata-skyalps-gtfs: make odh base url configurable

--- flighdata-skyalps-gtfs/src/main/java/com/opendatahub/rest/FlightsRest.java
+++ flighdata-skyalps-gtfs/src/main/java/com/opendatahub/rest/FlightsRest.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,25 +18,31 @@ import org.springframework.web.client.RestTemplate;
 
 import com.opendatahub.dto.Flights;
 
+@Service
 public class FlightsRest {
 
-	private static final String URL_GET_FLIGHTS = "https://mobility.api.opendatahub.com/v2/flat,node/Flight?limit=-1&where=sactive.eq.true";
-	private static final Logger LOG = LoggerFactory.getLogger(FlightsRest.class);
+	@Value("${odh.base_url}")
+	private String baseUrl; 
+
+	private final String GET_FLIGHTS = "/flat,node/Flight?limit=-1&where=sactive.eq.true";
+	private final Logger LOG = LoggerFactory.getLogger(FlightsRest.class);
 
 	@RequestMapping(value = "/getFlights", method = RequestMethod.POST)
 	@ResponseBody
-	public static Flights getFlights(RestTemplate restTemplate) {
+	public Flights getFlights(RestTemplate restTemplate) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.setAccept(Arrays.asList(MediaType.ALL)); /* Collections.singletonList(MediaType.APPLICATION_JSON) */
-		ResponseEntity<Flights> response = restTemplate.getForEntity(URL_GET_FLIGHTS, Flights.class);
+
+		String url = baseUrl + GET_FLIGHTS;
+		ResponseEntity<Flights> response = restTemplate.getForEntity(url, Flights.class);
 		if (response.getStatusCode() == HttpStatus.OK) {
-			LOG.info("Request Successful {}", URL_GET_FLIGHTS);
+			LOG.info("Request Successful {}", url);
 			LOG.debug("" + response.getBody());
 			return (response.getBody());
 
 		} else {
-			LOG.info("Request Failed {}", URL_GET_FLIGHTS);
+			LOG.info("Request Failed {}", url);
 			LOG.debug("" + response.getStatusCode());
 		}
 		return (null);
