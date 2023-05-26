ed7ca3d3631fa9ac02f69df4f5af978045e16ea7
Simon Dalvai <s.dalvai@noi.bz.it>
2023-05-26 15:30:45 +0200

    flighdata-skyalps-gtfs: s3 upload wait for completition

--- flighdata-skyalps-gtfs/src/main/java/com/opendatahub/utils/S3FileUtil.java
+++ flighdata-skyalps-gtfs/src/main/java/com/opendatahub/utils/S3FileUtil.java
@@ -12,6 +12,8 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
+
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,51 +25,54 @@ import org.springframework.stereotype.Component;
 @Component
 public class S3FileUtil {
 
-    private Logger logger = LoggerFactory.getLogger(S3FileUtil.class);
+        private Logger logger = LoggerFactory.getLogger(S3FileUtil.class);
 
-    @Value("${aws.access-key-id}")
-    private String accessKeyId;
+        @Value("${aws.access-key-id}")
+        private String accessKeyId;
 
-    @Value("${aws.bucket-name}")
-    private String bucketName;
+        @Value("${aws.bucket-name}")
+        private String bucketName;
 
-    @Value("${aws.access-secret-key}")
-    private String accessSecretKey;
+        @Value("${aws.access-secret-key}")
+        private String accessSecretKey;
 
-    /**
-     * Uploads a fileInputStream to a S3 bucket
-     *
-     * @param fileInputStream
-     * @param fileName
-     * @param contentLength
-     * @param lastModified
-     * @throws AmazonServiceException
-     * @throws AmazonClientException
-     * @throws InterruptedException
-     */
-    public void uploadFile(InputStream fileInputStream, String fileName, int contentLength)
-            throws AmazonServiceException, AmazonClientException, InterruptedException {
+        /**
+         * Uploads a fileInputStream to a S3 bucket
+         *
+         * @param fileInputStream
+         * @param fileName
+         * @param contentLength
+         * @param lastModified
+         * @throws AmazonServiceException
+         * @throws AmazonClientException
+         * @throws InterruptedException
+         */
+        public void uploadFile(InputStream fileInputStream, String fileName, int contentLength)
+                        throws AmazonServiceException, AmazonClientException, InterruptedException {
 
-        logger.debug("upload of file: {} to S3", fileName);
+                logger.debug("upload of file: {} to S3", fileName);
 
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKeyId, accessSecretKey);
+                BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKeyId, accessSecretKey);
 
-        AmazonS3 amazonS3 = AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                .withRegion(Regions.EU_WEST_1)
-                .build();
+                AmazonS3 amazonS3 = AmazonS3ClientBuilder
+                                .standard()
+                                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                                .withRegion(Regions.EU_WEST_1)
+                                .build();
 
-        TransferManager transferManager = TransferManagerBuilder.standard()
-                .withS3Client(amazonS3)
-                .build();
+                TransferManager transferManager = TransferManagerBuilder.standard()
+                                .withS3Client(amazonS3)
+                                .build();
 
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentLength(contentLength);
+                ObjectMetadata objectMetadata = new ObjectMetadata();
+                objectMetadata.setContentLength(contentLength);
 
-        transferManager.upload(bucketName, fileName, fileInputStream, objectMetadata);
+                Upload upload = transferManager.upload(bucketName, fileName, fileInputStream, objectMetadata);
+                upload.waitForCompletion();
 
-        logger.debug("upload of file: {} to S3 done", fileName);
-    }
+                transferManager.shutdownNow(false);
+
+                logger.debug("upload of file: {} to S3 done", fileName);
+        }
 
 }
