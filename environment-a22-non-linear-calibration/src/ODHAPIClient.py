0ca3d2673e93547d4d472170362789d328f9779d
Clemens Zagler <c.zagler@noi.bz.it>
2023-06-14 16:05:40 +0200

    env a22 nlc: Issue #27. - Refactored code to only download data that is actually needed (both stations and datatypes) - Directly load CSV instead of requiring translation in JSON via tool - Add new calibration coefficients - Modify NO2 formula, maintain old formula for old records (new field in processorParameters.csv)

--- environment-a22-non-linear-calibration/src/ODHAPIClient.py
+++ environment-a22-non-linear-calibration/src/ODHAPIClient.py
@@ -6,6 +6,7 @@ import requests
 import os
 import logging
 from ODHKeyCloakClient import KeycloakClient
+from functools import reduce
 
 log = logging.getLogger()
 logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO"))
@@ -26,9 +27,18 @@ class DataFetcher:
         else:
             return r.json()
 
-    def get_newest_data_timestamps(self):
-        response = self.fetch_data(
-            "/tree/EnvironmentStation/*/latest?limit=-1&where=sactive.eq.true,sorigin.eq.a22-algorab,mperiod.eq.3600&select=mvalidtime,scode&limit=-1")
+    def get_newest_data_timestamps(self, stations, types):
+        # each type is requested with _raw and _processed postfix, e.g. "NO2" becomes "NO2_raw,NO2_processed"
+        types_to_fetch = ",".join([tp + postfix for tp in types for postfix in ["_raw", "_processed"]])
+        station_filter = ",scode.in.(" + ",".join(stations) + ")"
+        response = self.fetch_data(f"/tree/EnvironmentStation/{types_to_fetch}/latest"
+            "?select=mvalidtime,scode"
+            "&where=sactive.eq.true"
+                ",sorigin.eq.a22-algorab"
+                ",mperiod.eq.3600"
+                f"{station_filter}"
+            "&limit=-1"
+            )
         if (response != None):
             log.debug("fetched newest data for raw and processed data:" + str(response))
             stations = response['data']['EnvironmentStation']['stations']
@@ -46,22 +56,27 @@ class DataFetcher:
             log.debug("Generated station map: " + str(station_map))
             return station_map
 
-    def get_raw_history(self, station_id, start, end):
+    def get_raw_history(self, station_id, start, end, types):
         raw_data_map = {}
-        data = self.fetch_data( "/flat/EnvironmentStation/*/"
-        + str(start).replace(" ","T") + "/"+ str(end).replace(" ","T")
-        + "?limit=-1&where=sactive.eq.true,sorigin.eq.a22-algorab,mperiod.eq.3600,scode.eq."
-        + station_id + "&select=mvalidtime,mvalue,tname")['data']
-        if data != None:
+        types_str = ",".join(map(lambda x : x + "_raw", types))
+        start_str = str(start).replace(" ","T")
+        end_str = str(end).replace(" ","T")
+        data = self.fetch_data( f"/flat/EnvironmentStation/{types_str}/{start_str}/{end_str}"+
+            "?select=mvalidtime,mvalue,tname"
+            "&where=sactive.eq.true"
+                ",sorigin.eq.a22-algorab"
+                ",mperiod.eq.3600"
+                ",scode.eq." + station_id + 
+            "&limit=-1"
+        )['data']
+        if data:
             log.debug("fetched history data:" + str(data))
             for record in data:
-                type_arr = str(record['tname']).split("_")
-                if len(type_arr)>1 and type_arr[1] == "raw":
-                    value = record['mvalue']
-                    type_id = type_arr[0]
-                    time = record['mvalidtime']
-                    typeMap = {}
-                    typeMap[type_id] = value
-                    raw_data_map.setdefault(time,{}).update(typeMap)
+                value = record['mvalue']
+                type_id = str(record['tname']).split("_")[0]
+                time = record['mvalidtime']
+                typeMap = {}
+                typeMap[type_id] = value
+                raw_data_map.setdefault(time,{}).update(typeMap)
         log.debug("Raw history: " + str(raw_data_map))
         return raw_data_map
