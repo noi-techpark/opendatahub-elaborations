394cf184083ebd5aa12cb6b8388f55fca6c90f19
Simon Dalvai <s.dalvai@noi.bz.it>
2024-01-23 15:59:33 +0100

    flighdata-skyalps-gtfs: fix type error

--- flighdata-skyalps-gtfs/src/main/java/com/opendatahub/service/GTFSCheckCalendar.java
+++ flighdata-skyalps-gtfs/src/main/java/com/opendatahub/service/GTFSCheckCalendar.java
@@ -6,6 +6,7 @@ package com.opendatahub.service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.opendatahub.validation.CheckLocationType;
 import com.opendatahub.dto.CalendarValues;
@@ -24,7 +25,7 @@ public class GTFSCheckCalendar extends Exception{
 	}
 
 
-	public static boolean checkcalendarValues(ArrayList<CalendarValues> calendarvalues) throws GTFSCheckCalendar  {
+	public static boolean checkcalendarValues(List<CalendarValues> calendarvalues) throws GTFSCheckCalendar  {
 		for(int i = 0; i < calendarvalues.size(); i++) {
 		if(calendarvalues.get(i).getService_id() != null && calendarvalues.get(i).getEnd_date() != null && String.valueOf(calendarvalues.get(i).getMonday()) != null && String.valueOf(calendarvalues.get(i).getTuesday()) != null && String.valueOf(calendarvalues.get(i).getWednesday()) != null && String.valueOf(calendarvalues.get(i).getThursday()) != null && String.valueOf(calendarvalues.get(i).getFriday()) != null && String.valueOf(calendarvalues.get(i).getSaturday()) != null && calendarvalues.get(i).getStart_date() != null) {
 			if(!calendarvalues.get(i).getService_id().toString().isBlank() && !calendarvalues.get(i).getEnd_date().toString().isBlank() && !String.valueOf(calendarvalues.get(i).getMonday()).isBlank() && !String.valueOf(calendarvalues.get(i).getTuesday()).isBlank() && !String.valueOf(calendarvalues.get(i).getWednesday()).isBlank() && !String.valueOf(calendarvalues.get(i).getThursday()).isBlank() && !String.valueOf(calendarvalues.get(i).getFriday()).isBlank() && !String.valueOf(calendarvalues.get(i).getSaturday()).isBlank() && !String.valueOf(calendarvalues.get(i).getSunday()).isBlank() && !calendarvalues.get(i).getStart_date().isBlank()) {
