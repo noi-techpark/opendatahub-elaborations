262d4eafbdd473a8bf8b31080cb52a7635e1cb1d
Clemens Zagler <c.zagler@noi.bz.it>
2024-02-06 15:20:37 +0100

    ci/cd and cron

--- traffic-a22-data-quality/src/go.mod
+++ traffic-a22-data-quality/src/go.mod
@@ -10,10 +10,6 @@ require github.com/go-co-op/gocron v1.37.0
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
-	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
-	golang.org/x/text v0.3.7 // indirect
 )
--- traffic-a22-data-quality/src/main.go
+++ traffic-a22-data-quality/src/main.go
@@ -5,24 +5,28 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+	"time"
 	"traffic-a22-data-quality/dc"
 	"traffic-a22-data-quality/log"
+
+	"github.com/go-co-op/gocron"
 )
 
 func main() {
 	log.InitLogger()
 
-	/* cron := os.Getenv("SCHEDULER_CRON")
+	cron := os.Getenv("SCHEDULER_CRON")
 	slog.Debug("Cron defined as: " + cron)
 
 	if len(cron) == 0 {
 		slog.Error("Cron job definition in env missing")
 		os.Exit(1)
-	} */
+	}
 
 	dc.Job()
-	// start cron job
-	// s := gocron.NewScheduler(time.UTC)
-	// s.CronWithSeconds(cron).Do(dc.Job)
-	// s.StartBlocking()
+	s := gocron.NewScheduler(time.UTC)
+	s.CronWithSeconds(cron).Do(dc.Job)
+	s.StartBlocking()
 }
