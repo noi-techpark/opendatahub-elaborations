1282e02f7d8d6c3061ec2fb02a11e573c9fa6284
Clemens Zagler <c.zagler@noi.bz.it>
2024-02-20 09:42:01 +0100

    traffic-a22: enhance checks.sql

--- traffic-a22-data-quality/checks.sql
+++ traffic-a22-data-quality/checks.sql
@@ -2,7 +2,7 @@
 -- SPDX-License-Identifier: CC0-1.0
 
 /* Check consistency for total and equiv data types */
-select ml.timestamp, s.stationcode, s.stationtype,
+select ml.timestamp, s.stationcode, p.stationcode as parent, s.stationtype, 
 ml.double_value as "light", 
 mh.double_value as "heavy",
 mb.double_value as "bus",
@@ -11,6 +11,7 @@ me.double_value as "equiv",
 (ml.double_value + mh.double_value + mb.double_value) as calc_total,
 (ml.double_value + mh.double_value * 2.5 + mb.double_value * 2.5) as calc_equiv
 from station s
+left outer join station p on p.id = s.parent_id
 join type l on l.cname = 'Nr. Light Vehicles'
 join measurement ml on ml.type_id = l.id and ml.station_id = s.id and ml.period = 86400
 join type h on h.cname = 'Nr. Heavy Vehicles'
@@ -21,5 +22,5 @@ join type e on e.cname = 'Nr. Equivalent Vehicles'
 join measurement me on me.type_id = e.id and me.station_id = s.id and me.period = 86400 and me.timestamp = ml.timestamp
 join type t on t.cname = 'Nr. Vehicles'
 join measurement mt on mt.type_id = t.id and mt.station_id = s.id and mt.period = 86400 and mt.timestamp = ml.timestamp
-where stationcode like 'A22:%' and stationtype in ('TrafficDirection','TrafficSensor')
+where s.stationcode like 'A22:%' and s.stationtype in ('TrafficDirection','TrafficSensor')
 order by s.stationcode, ml.timestamp desc
