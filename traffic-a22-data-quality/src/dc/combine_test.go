a626f325883baf0f24c902e8cba11cfaf43a61a5
Clemens Zagler <c.zagler@noi.bz.it>
2026-03-13 14:27:03 +0100

    feat(a22-data-quality): add top level location station

--- traffic-a22-data-quality/src/dc/combine_test.go
+++ traffic-a22-data-quality/src/dc/combine_test.go
@@ -46,6 +46,9 @@ func TestCombineLocations(t *testing.T) {
 }
 
 func TestCombine(t *testing.T) {
+	s1871_nord.ParentStation = "A22:1871"
+	s1872_sud.ParentStation = "A22:1872"
+
 	directionStationType = "TrafficDirection"
 	r := combineDirections([]bdplib.Station{s1871_1, s1871_2})
 	if !reflect.DeepEqual(r[0], s1871_nord) {
