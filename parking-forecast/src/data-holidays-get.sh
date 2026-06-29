37798cfcef779d044e5ee695ac2f336fdfcbbee1
Jeffrey Ofori Kwakye <jeffkwakye4u@gmail.com>
2026-06-29 17:43:04 +0000

    script to read the latest holiday date from existing csv

---
+++ parking-forecast/src/data-holidays-get.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2021-2025 STA AG <info@sta.bz.it>
+# SPDX-FileContributor: Chris Mair <chris@1006.org>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+csv=data-holidays/holidays.csv
+
+# If we already have data, extract the last date and pass it to the Python script
+# so it only fetches new data from that point on
+if [ -f $csv ]; then
+    line_count=$(wc -l < "$csv")
+    if [ "$line_count" -gt 1 ]; then
+        datefrom=`tail -n 1 "$csv" | cut -d',' -f1`
+        echo "Existing holidays file was found. Starting request from latest date $datefrom"
+    fi
+fi
+
+# Call the Python script to fetch and generate the holidays CSV
+python3 data-holidays-get.py
+
+echo "Job done"
